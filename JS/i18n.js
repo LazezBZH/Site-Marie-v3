39668c5432337703cc7540ee850f2a76c0f92502
LazezBZH <lazez.bzh@caramail.fr>
2023-08-13 01:55:44 +0200

    localisation ok + loader

--- JS/i18n.js
+++ JS/i18n.js
@@ -1,32 +1,52 @@
-// $(".lang-switcher").on("click", function () {
-//   var $html = $("html");
-
-//   if ($html.attr("lang") === "fr-fr") {
-//     $html.attr("lang", "en-gb");
-//   } else {
-//     $html.attr("lang", "fr-fr");
-//   }
-// });
-
-// const langSwitcher = document.querySelector(".lang-switcher");
-// const html = document.querySelector("html");
-// document.addEventListener("DOMContentLoaded", function () {
-//   langSwitcher.addEventListener("click", function () {
-//     if (html.getAttribute("lang") === "fr-fr") {
-//       html.setAttribute("lang", "en-gb");
-//     } else {
-//       html.setAttribute("lang", "fr-fr");
-//     }
-//   });
-// });
-
 const langSwitcher = document.querySelector(".lang-switcher");
 const html = document.querySelector("html");
+const contact2 = document.querySelector(".i18n-contact2");
+const contact4 = document.querySelector(".i18n-contact4");
+const loader = document.querySelector(".loader");
+const header = document.querySelector("header");
+
+if (!localStorage.getItem("lang")) {
+  localStorage.setItem("lang", "fr-fr");
+}
+const chosenLang = localStorage.getItem("lang");
+console.log(chosenLang);
+
+window.addEventListener("load", setLang);
+
+function setLang() {
+  loader.style.display = "block";
+  window.scrollTo(0, 0);
+
+  setTimeout(function () {
+    loader.style.display = "none";
+    header.style.zIndex = "999";
+  }, 1700);
+  html.setAttribute("lang", chosenLang);
+  if (html.getAttribute("lang") === "fr-fr") {
+    contact2.innerHTML = `<a href="https://forms.gle/uNt3CuQX9QPtZv318"><img  src="/images/formfr.jpg" alt="">
+</a>`;
+    contact4.innerHTML = `<a class="btn btn-icon btn-phone" href="tel:+33650335412"><i class="fas fa-phone"></i><span>+33650335412</span></a>`;
+  } else if (html.getAttribute("lang") === "en-gb") {
+    contact2.innerHTML = `<a href="https://forms.gle/42yqmJ26zecSsn5i9">
+  <img  src="/images/formen.jpg" alt="">
+</a>`;
+    contact4.innerHTML = `<a class="btn btn-icon btn-phone" href="tel:+353870623425"><i class="fas fa-phone"></i><span>+353870623425</span></a>`;
+  }
+}
 
 langSwitcher.addEventListener("click", function () {
   if (html.getAttribute("lang") === "fr-fr") {
     html.setAttribute("lang", "en-gb");
+    localStorage.setItem("lang", "en-gb");
+    contact2.innerHTML = `<a href="https://forms.gle/42yqmJ26zecSsn5i9">
+  <img  src="/images/formen.jpg" alt="">
+</a>`;
+    contact4.innerHTML = `<a class="btn btn-icon btn-phone" href="tel:+353870623425"><i class="fas fa-phone"></i><span>+353870623425</span></a>`;
   } else {
     html.setAttribute("lang", "fr-fr");
+    localStorage.setItem("lang", "fr-fr");
+    contact2.innerHTML = `<a href="https://forms.gle/uNt3CuQX9QPtZv318"><img  src="/images/formfr.jpg" alt="">
+</a>`;
+    contact4.innerHTML = `<a class="btn btn-icon btn-phone" href="tel:+33650335412"><i class="fas fa-phone"></i><span>+33650335412</span></a>`;
   }
 });
---
+++ JS/loader.js
@@ -0,0 +1,51 @@
+let zen = function () {
+  const logo = Snap.select(".logo-loader");
+  logo.attr({
+    fill: "#AED5FF",
+  });
+  logo.select(".zenman").transform("s.2,t98,105");
+  logo
+    .text(20, 10, "Bienvenue / Welcome")
+    .attr({
+      fill: "#874010",
+      "font-family": "Varela Round",
+      "text-shadow": "0 0 52px #000",
+    })
+    .transform("t-19,89.5");
+
+  const circle = Snap.select(".circle");
+
+  const svgs = [circle];
+
+  const rot = function (thisCircle, per, dir) {
+    const dots = thisCircle.select(".dots");
+    const scale = dots.transform().localMatrix.d;
+    dots.stop().animate(
+      {
+        transform: "s" + scale + ", t0,0r" + 360 * dir,
+      },
+      100000 * (per / 100),
+      function () {
+        dots.transform("s" + scale + ",t0,0r0");
+        rot(thisCircle, per);
+      }
+    );
+  };
+
+  let rings = 5;
+  for (let i = 0; i <= rings; i++) {
+    svgs.push(circle.clone());
+    let per = i / rings;
+
+    let speed = Math.floor(Math.random() * 100) + 25;
+    let dir = Math.random() < 0.5 ? -1 : 1;
+    let scale = per * 0.9;
+    svgs[i].attr({
+      fill: "#874010",
+    });
+    svgs[i].select(".dots").transform("s" + scale + ",t0,0r0");
+
+    rot(svgs[i], speed, dir);
+  }
+};
+zen();
